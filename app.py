b1bb24c3cad8f78dc72984ffd1c903e557772972
rameshlanke <66162970+Rameshlanke@users.noreply.github.com>
2026-03-30 17:41:03 +0530

    Change Flask app to run in production mode

--- app.py
+++ app.py
@@ -530,5 +530,5 @@ def self_edit_collaborator():
 
 # ---------------- RUN ----------------
 if __name__ == "__main__":
-    app.run(debug=True, port = 7000)
+    app.run(debug=False, port = 9123)
 
